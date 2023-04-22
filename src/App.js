e16e9462b6e0b4e93d6b9cd2d625e9a7c5fc2909
Jackie <jackieshields01@gmail.com>
2023-04-22 12:06:07 +0100

    added boostrap

--- src/App.js
+++ src/App.js
@@ -4,21 +4,23 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is created by{" "}
-        <a href="https://shieldstalent.com" target="_blank" rel="noreferrer">
-          Jackie
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/jackiedelrey/weather-app-final-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is created by{" "}
+          <a href="https://shieldstalent.com" target="_blank" rel="noreferrer">
+            Jackie
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/jackiedelrey/weather-app-final-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }

6d1412d177887c026a5361c36daf08c5395f676f
Jackie <jackieshields01@gmail.com>
2023-04-22 11:42:58 +0100

    Add the Github Repo

--- src/App.js
+++ src/App.js
@@ -2,5 +2,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is created by{" "}
+        <a href="https://shieldstalent.com" target="_blank">
+          Jackie
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/jackiedelrey/weather-app-final-react"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }

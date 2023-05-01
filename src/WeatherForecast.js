c333e20f805df4399505f8fcd8a88095f5b328e0
Jackie <jackieshields01@gmail.com>
2023-05-01 11:16:11 +0100

    fixed bug

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -6,23 +6,23 @@ import axios from "axios";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
     console.log(response.data);
     setForecast(response.data.daily);
     setLoaded(true);
   }
+
   if (loaded) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          {forecast.map(function (dailyForecast, index) {
-            if (index < 5) {
-              return (
-                <div className="col" key={index}>
-                  <WeatherForecastDay data={dailyForecast} />
-                </div>
-              );
-            }
+          {forecast.slice(0, 5).map(function (dailyForecast, index) {
+            return (
+              <div className="col" key={index}>
+                <WeatherForecastDay data={dailyForecast} />
+              </div>
+            );
           })}
         </div>
       </div>
